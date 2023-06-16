5cc4919ae7a76effea2c2534b16d01d371ef9292
cyberpwn <danielmillst@gmail.com>
2023-06-15 20:27:25 -0400

    f

--- api/src/main/java/art/arcane/bridge/api/Bridge.java
+++ api/src/main/java/art/arcane/bridge/api/Bridge.java
@@ -1,4 +1,29 @@
 package art.arcane.bridge.api;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Bridge {
+    public static String readResource(String jarResourcePath) {
+        try (InputStream in = Bridge.class.getResourceAsStream("/" + jarResourcePath);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            String l;
+            StringBuilder content = new StringBuilder();
+
+            while ((l = reader.readLine()) != null) {
+                {
+                    content.append(l);
+                }
+            }
+
+            return content.toString();
+        }
+        catch (IOException e) {
+            // Handle exception
+        }
+
+        return null;
+    }
 }
---
+++ bukkit/src/main/java/art/arcane/bridge/Main.java
@@ -0,0 +1,7 @@
+package art.arcane.bridge;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
